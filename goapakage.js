102a60d2c4340bf5e89acd0853841165f1fea09d
Subhashreemohanty07 <138459225+Subhashreemohanty07@users.noreply.github.com>
2023-07-24 00:46:15 +0530

    Update goapakage.js
    adding the darkmode

--- goapakage.js
+++ goapakage.js
@@ -9,3 +9,25 @@ buttons.forEach( button =>{
         icon.classList.toggle('rotate');
     })
 } )
+
+//darkmode light mode code with chane in icon
+function darkmod()
+{
+
+    const dk=document.getElementById('dark-btn');
+    if(dk.className=='fa-solid fa-moon')
+    {
+    const el=document.body;
+    el.classList.toggle("dark-mode");
+    dk.className='fa-sharp fa-solid fa-moon';
+    dk.style.color='orange';
+    }
+    else{
+        const el=document.body;
+        el.classList.toggle("dark-mode");
+        dk.className='fa-solid fa-moon';
+        dk.style.color='white';
+    }
+
+
+ }
