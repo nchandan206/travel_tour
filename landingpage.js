1ab2a4acb12ade6a2c4fd5b9895a68fa7d9c8b43
Subhashreemohanty07 <138459225+Subhashreemohanty07@users.noreply.github.com>
2023-07-24 11:04:03 +0530

    Update landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -1,1 +1,27 @@
+//darkmode light mode code with chane in icon
+function darkmod()
+{
 
+    const dk=document.getElementById('dark-btn');
+    if(dk.className=='fa-solid fa-moon')
+    {
+    const el=document.body;
+    el.classList.toggle("dark-mode");
+    dk.className='fa-sharp fa-solid fa-moon';
+    dk.style.color='orange';
+    }
+    else{
+        const el=document.body;
+        el.classList.toggle("dark-mode");
+        dk.className='fa-solid fa-moon';
+        dk.style.color='white';
+    }
+
+
+ }
+//submit successful message
+function sub()
+{
+    alert('we will get back soon');
+    document.location.href='#';
+}
